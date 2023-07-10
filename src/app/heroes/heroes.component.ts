9a76fc448c953248a6a2a83e9d6300d2b2d43c74
juantobarra150 <delarosa.juanluis@gmail.com>
2023-07-10 12:41:46 +0200

    Initial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+
+heroes = HEROES;
+selectedHero ?: Hero;
+
+hero: Hero = {
+  id: 1, 
+  name: 'Windstorm'
+};
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+ }
+}
